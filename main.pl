50293e619d6f731f0a488b7a526298f1752ca7b1
p-hebert <philippe.hebert.concordia@outlook.com>
2015-03-26 20:49:15 -0400

    Included rules from slides simulating_automata.pdf to the main prolog database

--- main.pl
+++ main.pl
@@ -1,1 +1,26 @@
-//TODO: assignment
+%rules
+
+%from slides
+
+composite_state(S) :- findall(Composite,
+superstate(Composite, _), L),
+list_to_set(L, S).
+nested_state(S) :- superstate(_, S).
+events_per_state(State, EventSet) :-
+    findall(Event,
+		(superstate(State, Substate),
+		transition(Substate, _, Event, _, _)),
+		EventList),
+	list_to_set(EventList, EventSet).
+included_states(S, L) :- findall(State,
+(state(State), superstate(S, State)), L).
+reachable(Target, Source) :- transition(Source,
+Target, _, _, _).
+reachable(Target, Source) :- transition(Source,
+Node, _, _, _),
+reachable(Target, Node).
+events(EventSet) :- findall(Event,
+transition(_, _, Event, _, _), EventList),
+list_to_set(EventList, EventSet).
+
+%extended rules
