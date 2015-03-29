85704b7783d2a3f341b036a33448ce7e04bd67f8
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2015-03-29 17:34:29 -0400

    Built part 5

---
+++ part5.pl
@@ -0,0 +1,21 @@
+%facts
+%states
+state(error_rcv).
+state(applicable_rescue).
+state(reset_module_data).
+superstate(error_diagnosis, error_rcv).
+superstate(error_diagnosis, applicable_rescue).
+superstate(error_diagnosis, reset_module_data).
+
+%events
+event(apply_protocol_rescues).
+event(reset_to_stable).
+
+%transition(state1,state2,event,guard,action)
+transition(error_rcv, applicable_rescue, null, 'err_protocol_def', null).
+transition(error_rcv, reset_module_data, null, !'err_protocol_def', null).
+transition(applicable_rescue, exit, apply_protocol_rescues, null, null).
+transition(reset_module_data, exit, reset_to_stable, null, null)
+
+%var
+var(err_protocol_def, bool).
