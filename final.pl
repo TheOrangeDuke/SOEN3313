93d267d613fe0faa9b97b39605dddb4585487427
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2015-03-30 12:02:48 -0400

    fake prolog

--- final.pl
+++ final.pl
@@ -207,40 +207,77 @@ transition(_, _, Event, _, _), EventList),
 list_to_set(EventList, EventSet).
 
 %! extended rules
+
+
 %! 1
+is_loop(Event, Guard) :- transition(StateA, StateA, Event, Guard, _).
 %! 2
 all_loops(Set) :- findall([Event,Guard], is_loop(Event,Guard), LoopList),
                   list_to_set(LoopList, Set).
+
+
+
 %! 3
+is_edge(Event, Guard) :- transition(_,_,Event,Guard,_).
 %! 4
 size(Length) :- findall([Event,Guard], is_edge(Event,Guard), EdgeList),
                 list_to_set(EdgeList,EdgeSet),
                 length(EdgeSet,Length).
+
+
+
 %! 5
+is_link(Event, Guard) :- is_edge(Event, Guard) AND !is_loop(Event, Guard).
 %! 6
 all_superstates(Set) :- findall(State, superstate(State,_), SuperList),
                         list_to_set(SuperList,Set).
+
+
+
 %! 7
+ancestor(Ancestor, Descendant) :- findall([Ancestor, Descendant],
+                                        transition(Ancestor, Descendant,_,_,_),
+                                        Ancestor).
 %! 8
 inheritss_transitions(State,List) :-
                     findall([Super,Other,Event,Guard,Action],
                     (superstate(Super,State), transition(Super,Other,Event,Guard,Action)),
                      List).
+
+
+
 %! 9
+all_states(L) :- findall(State, state(State), L).
 %! 10
 all_init_states(L) :- findall(State, init_state(State), L).
 %! 11
+%! getting the state that is never transitioned to...
+get_starting_state(State) :- .
+
+
+
 %! 12
 state_is_reflexive(State) :- transition(State,State,_,_,_).
 %! 13
+%! I cannot even understand why
+graph_is_reflexive :- .
+
+
+
 %! 14
 get_guards(Ret) :-  findall(Guard, guard(Guard), Ret).
 %! 15
 get_events(Ret) :-  findall(Event, event(Event), Ret).
 %! 16
 get_actions(Ret) :-  findall(Action, action(Action), Ret).
-%! 17
 
+
+
+
+%! 17
+get_only_guarded(Ret) :- findall([StateA,StateB],
+                            transition(StateA, StateB, null, Guard, null),
+                            Ret).
 %! 18
 legal_events_of(State,L) :- findall(Event,
                             transition(State,_,Event,_,_),
